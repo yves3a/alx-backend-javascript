b7ee11932da986f697d4f1bbe50d8f63b365f77e
yves3a <yves.irakoz3@gmail.com>
2025-07-03 09:50:26 +0200

    updates

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,33 +1,55 @@
+#!/usr/bin/env node
+
 const express = require('express');
 
-const app = express();
 const port = 7865;
 
+const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`);
+  const { id } = req.params;
+  res.status(200).send(`Payment methods for cart ${id}`);
 });
 
 app.get('/available_payments', (req, res) => {
-  const obj = {
-    payment_methods: {
-      credit_cards: true,
-      paypal: false,
+  res.status(200).json(
+    {
+      payment_methods: {
+        credit_cards: true,
+        paypal: false,
+      },
     },
-  };
-  res.json(obj);
+  );
 });
 
 app.post('/login', (req, res) => {
-  const username = req.body.userName;
-  res.end(`Welcome ${username}`);
+  if (!req.body) {
+    res.status(401).send('Invalid credentials');
+  } else {
+    try {
+      const { userName } = req.body;
+      if (userName.length === 0) {
+        res.status(401).send('Invalid credentials');
+      }
+      res.status(200).send(`Welcome ${userName}`);
+    } catch (error) {
+      res.status(401).send('Invalid credentials');
+    }
+  }
+});
+
+// Handle undefined routes and general 404 page returns
+app.use((req, res) => {
+  res.status(404).send('Not found');
 });
 
 app.listen(port, () => {
-  console.log('API available on localhost port 7865');
+  console.log(`The server is listening on port ${port}`);
 });
+
+module.exports = app;
